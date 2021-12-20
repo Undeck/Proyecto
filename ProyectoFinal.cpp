ff9aed05ced1f7ae6b527b23cfee42d2aebf8e1f
Undeck <65004793+Undeck@users.noreply.github.com>
2021-12-19 19:36:33 -0600

    Add files via upload

---
+++ ProyectoFinal.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+	
+    printf("PROYECTO FINAL DE INTRODUCCION A LA COMPUTACION\n");
+    printf("Calculadora de Sumas y Restas\n");
+    printf("Porfavor seleccione 1 para suma y 2 para resta\n");
+    
+
+    return 0;
+}
